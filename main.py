b4bee86574b638ff7032a07ce3306f95b172b2b5
Leonid Litvinov <leonidlitvinoff@yandex.ru>
2021-10-23 21:38:39 +0700

    Code cleanup

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 from flask import Flask, render_template, request, redirect, jsonify, send_file
-from werkzeug.datastructures import FileStorage
 import json
-from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QVBoxLayout, QApplication
-from PyQt5.QtWebEngineWidgets import QWebEnginePage, QWebEngineView
-from PyQt5.QtCore import QUrl, Qt
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QMouseEvent
-from typing import List
-import nmap
 import os
-import pythonping
 from pathlib import Path
-import base64
 from threading import Thread
 import sys
 import requests
-import time
 import subprocess
 from flask_cors import CORS
 from contextlib import contextmanager
 import socket
+
 
 @contextmanager
 def socketcontext(*args, **kw):
@@ -28,50 +23,40 @@ def socketcontext(*args, **kw):
     finally:
         s.close()
 
+
 def get_my_ip():
     with socketcontext(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(("8.8.8.8", 80))
         return s.getsockname()[0]
 
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 IP = get_my_ip()
+
 
 class CustomQWebView(QWebEngineView):
     def mousePressEvent(self, a0: QMouseEvent) -> None:
         print(a0.button())
 
+
 class MainWindow(QMainWindow):
     def __init__(self, f_app: Flask, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        # widget = QWidget()
-        # label = QLabel("Flask is running...")
-        # self.layout = QVBoxLayout()
-        # self.browser = QWebEnginePage()
-        # # self.layout.addWidget(label)
-        # self.layout.addWidget(self.browser)
-        # widget.setLayout(self.layout)
-        # self.setCentralWidget(widget)
-        # vbox = QVBoxLayout(self)
         self.f_app = f_app
         self.webEngineView = CustomQWebView()
         self.setCentralWidget(self.webEngineView)
         with f_app.test_request_context("/"):
             self.webEngineView.setHtml(render_template("empty_loading.html", ip=f"http://{IP}:874/"))
-        # self.webEngineView.load(QUrl(f"http://{get_my_ip()}:874/"))
-        # vbox.addWidget(self.webEngineView)
-
-        # self.setLayout(vbox)
-        # Thread(target=while_flask_app_loaded, args=[f_app, self.webEngineView]).start()
 
 
 class Config(object):
     config = {}
-    q_app = None # type: QApplication
-    q_window = None # type: QMainWindow
-    f_app = None # type: Flask
+    q_app = None  # type: QApplication
+    q_window = None  # type: QMainWindow
+    f_app = None  # type: Flask
     path = ""
 
-    def __init__(self, path: str=None):
+    def __init__(self, path: str = None):
         if path:
             self.path = path
             self.config = self.load_config(path)
@@ -112,11 +97,7 @@ class Config(object):
             except:
                 return False
 
-# app = Flask(__name__)
 
-#
-# app.run("0.0.0.0", port=874)
-# print("MMMM")
 app_ = Flask(__name__)
 CORS(app_)
 connected_users = []
@@ -130,13 +111,7 @@ flaskThread = Thread(target=app_.run, daemon=True, kwargs=kwargs).start()
 
 @app_.route("/status.png")
 def status():
-    return send_file( open('static/img/status.png', 'rb'), mimetype="image/png")
-
-# @app_.route("/empty_loading")
-# def empty_loading():
-#     print(f"http://{IP}:874/")
-#     return render_template("empty_loading.html", ip=f"http://{IP}:874/")
-
+    return send_file(open('static/img/status.png', 'rb'), mimetype="image/png")
 
 
 @app_.route("/close_app")
@@ -160,6 +135,7 @@ def connect():
     except:
         return jsonify({'status': 'false'})
 
+
 @app_.route("/connect_to")
 def connect_to():
     print('cccc')
@@ -176,14 +152,17 @@ def connect_to():
     except Exception as e:
         return jsonify({'status': 'false', 'error': str(e)})
 
+
 @app_.route("/disconnect")
 def disconnect():
     if not 'addr' in request.args:
         return jsonify({'status': 'false'})
-    if request.remote_addr != request.args['addr'] or not request.args['addr'] in connected_users or not user_config["teacher"]:
+    if request.remote_addr != request.args['addr'] or not request.args['addr'] in connected_users or not user_config[
+        "teacher"]:
         return jsonify({'status': 'false'})
     connected_users.remove(request.args['addr'])
     return jsonify({'status': 'true'})
+
 
 @app_.route("/disconnect_from")
 def disconnect_from():
@@ -191,7 +170,8 @@ def disconnect_from():
     if request.host.split(':')[0] != request.remote_addr or connected_teacher == '' or user_config["teacher"]:
         return jsonify({'status': 'false'})
     try:
-        data = requests.get(f"http://{connected_teacher}:874/disconnect?addr={request.host.split(':')[0]}", timeout=0.5).json()
+        data = requests.get(f"http://{connected_teacher}:874/disconnect?addr={request.host.split(':')[0]}",
+                            timeout=0.5).json()
         if data['status'] == 'true':
             connected_teacher = ""
         return data
@@ -210,6 +190,7 @@ def join():
             pass
     return render_template("login.html")
 
+
 @app_.route('/find_local_users')
 def find_local_users():
     if request.host.split(':')[0] != request.remote_addr:
@@ -224,6 +205,7 @@ def find_local_users():
     user_config["last_scan"] = data
     return jsonify({"users_data": data, "connected_users": connected_users})
 
+
 @app_.route("/get_other_file")
 def get_other_file():
     if connected_teacher == request.remote_addr:
@@ -236,6 +218,7 @@ def get_other_file():
             pass
     return jsonify({'status': 'false'})
 
+
 @app_.route('/user_data')
 def usr_data():
     usr = {'connected_users': connected_users, "connected_teacher": connected_teacher}
@@ -243,15 +226,17 @@ def usr_data():
     if request.host.split(':')[0] == request.remote_addr:
         return jsonify(usr)
 
+
 @app_.route("/")
 def index():
     if request.host.split(':')[0] == request.remote_addr:
         if not user_config.__contains__("username"):
             return redirect("/join")
-        return render_template("index.html", user_config=user_config, connected_users=connected_users, active_item=0, last_scan=user_config["last_scan"], connected_teacher=connected_teacher)
+        return render_template("index.html", user_config=user_config, connected_users=connected_users, active_item=0,
+                               last_scan=user_config["last_scan"], connected_teacher=connected_teacher)
     return jsonify({"teacher": user_config["teacher"], "username": user_config["username"]})
 
-# print(request.files.to_dict())
+
 @app_.route("/files", methods=["GET", "POST"])
 def files():
     files = []
@@ -265,10 +250,12 @@ def files():
             a = requests.get(f"http://{connected_teacher}:874/files").json()
             if a['status'] == 'true':
                 other_data = a['files']
-        return render_template("files.html", active_item=1, user_config=user_config, files=files, other_data=other_data, connected_teacher=connected_teacher)
+        return render_template("files.html", active_item=1, user_config=user_config, files=files, other_data=other_data,
+                               connected_teacher=connected_teacher)
     elif user_config["teacher"] and request.remote_addr in connected_users:
         return jsonify({'status': 'true', 'files': files})
     return jsonify({"status": "false"})
+
 
 @app_.route('/open_file')
 def open_file():
@@ -280,6 +267,7 @@ def open_file():
     except:
         return jsonify({'status': 'false'})
 
+
 @app_.route('/select_file')
 def select_file():
     if request.host.split(':')[0] != request.remote_addr:
@@ -289,7 +277,6 @@ def select_file():
         return jsonify({'status': 'true'})
     except:
         return jsonify({'status': 'false'})
-
 
 
 @app_.route("/delete_file")
@@ -304,6 +291,7 @@ def delete_file():
     except:
         return jsonify({'status': 'false', 'error': 'Permission denied'})
 
+
 @app_.route('/get_file')
 def get_file():
     try:
@@ -313,6 +301,7 @@ def get_file():
     except Exception as e:
         print(e)
         return jsonify({'status': 'false', 'error': 'Permission denied'})
+
 
 @app_.route("/upload_file", methods=["POST"])
 def upload_file():
@@ -327,17 +316,16 @@ def upload_file():
 def sends_file():
     if request.host.split(':')[0] != request.remote_addr:
         return jsonify({'status': 'false', 'error': 'Permission denied'})
-    # requests.post(f"http://{request.host}/upload_file",
-    #               files={"upload_file": open(ROOT_DIR + "/SharedFiles/" + request.args['f_name'], "rb")})
     for i in connected_users:
         try:
-            # requests.get(f"http://{i}:874/get_other_file?f_name={request.args['f_name']}")
-            requests.post(f"http://{i}:874/upload_file", files={"upload_file": open(ROOT_DIR + "/SharedFiles/" + request.args['f_name'], "rb")})
+            requests.post(f"http://{i}:874/upload_file",
+                          files={"upload_file": open(ROOT_DIR + "/SharedFiles/" + request.args['f_name'], "rb")})
             pass
         except Exception as e:
             print(e)
             continue
     return jsonify({'status': 'true'})
+
 
 @app_.route('/notifies')
 def notifications():
@@ -352,28 +340,10 @@ def notifications():
         return jsonify({'status': 'false'})
 
 
-class PrintB(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.running = True
-    def run(self):
-        with user_config.f_app.test_request_context():
-            while self.running:
-                time.sleep(2)
-                user_config.q_window.webEngineView.load(QUrl(f"http://{IP}:874/"))
-                self.stop()
-    def stop(self):
-        self.running = False
-
 app = QApplication(sys.argv)
 user_config.q_app = app
 window = MainWindow(app_)
 user_config.q_window = window
 window.setWindowFlags(Qt.CustomizeWindowHint)
-# window.setWindowFlags(Qt.FramelessWindowHint | Qt.CustomizeWindowHint)
-# window.setWindowOpacity(0)
 window.show()
-# kwargs = {'': '0.0.0.0', 'port': 874, 'threaded': True, 'use_reloader': False, 'debug': False}
-# awaitFlaskThread = Thread(target=awaitStartFlask, daemon=True).start()
-# PrintB().start()
 app.exec_()
